694a229c4b909f861c410ddb3b1e0e998608acda
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-01-25 16:57:25 +0100

    update

--- app/updates/page.tsx
+++ app/updates/page.tsx
@@ -8,7 +8,7 @@ export default function UpdatesPage() {
             </section>
             <div className={styles.main}>
                 <section className={styles.section}>
-                    <h2>socialM Shorts Expansion</h2>
+                    <h2>socialM Shorts</h2>
                     <p>socialM Shorts se rychle rozrostly a staly se jednou z nejoblíbenějších funkcí na platformě. Tento nástroj je navržen tak, aby umožnil tvůrcům produkovat vertikální videa kratší než 60 sekund, což uživatelům poskytuje zábavný a poutavý zážitek. Vzhledem k tomu, že tuto funkci již využívají miliony tvůrců, společnost socialM zavedla nové možnosti zpeněžení, které tvůrcům umožňují získávat přímé příjmy z obsahu krátkých videí. To vytvořilo nové příležitosti pro influencery, marketéry i firmy. SocialM Shorts se ukázaly jako fantastický způsob, jak zvýšit zapojení publika a lépe uspokojit mobilní uživatele, kteří mají rádi rychlý a kousavý obsah.</p>
                 </section>
                 <section className={styles.section}>
